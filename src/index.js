83402e3a620dd79cb751e58dd9c843d2d31255fb
sky_de_1991 <79264045+Sky-De@users.noreply.github.com>
2023-08-24 01:12:03 +0330

    app/index js reStructured (#9)

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './css/global.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { CssBaseline } from "@mui/material";
+import { BrowserRouter as Router } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import "./css/global.css";
+import App from "./App";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <CssBaseline />
+      <App />
+    </Router>
   </React.StrictMode>
 );
