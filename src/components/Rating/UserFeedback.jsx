ae49efba2886a05cb277e4e6e4d10ca049354413
SkyDe <sky_de1991@outlook.com>
2023-08-24 21:44:27 +0330

    first deploy debug

--- src/components/Rating/UserFeedback.jsx
+++ src/components/Rating/UserFeedback.jsx
@@ -1,5 +1,4 @@
 import * as React from "react";
-import PropTypes from "prop-types";
 import { styled } from "@mui/material/styles";
 import Rating from "@mui/material/Rating";
 import SentimentVeryDissatisfiedIcon from "@mui/icons-material/SentimentVeryDissatisfied";
@@ -7,7 +6,6 @@ import SentimentDissatisfiedIcon from "@mui/icons-material/SentimentDissatisfied
 import SentimentSatisfiedIcon from "@mui/icons-material/SentimentSatisfied";
 import SentimentSatisfiedAltIcon from "@mui/icons-material/SentimentSatisfiedAltOutlined";
 import SentimentVerySatisfiedIcon from "@mui/icons-material/SentimentVerySatisfied";
-
 const StyledRating = styled(Rating)(({ theme }) => ({
   "& .MuiRating-iconEmpty .MuiSvgIcon-root": {
     color: theme.palette.action.disabled,
@@ -41,10 +39,6 @@ function IconContainer(props) {
   const { value, ...other } = props;
   return <span {...other}>{customIcons[value].icon}</span>;
 }
-
-IconContainer.propTypes = {
-  value: PropTypes.number.isRequired,
-};
 
 export default function UserFeedback() {
   return (
