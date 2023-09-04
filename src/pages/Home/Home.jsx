9d98ac84e1c8e425465f7f10e8c7db5256990481
Anubhav Adhikari <53504460+anubhav1206@users.noreply.github.com>
2023-09-04 15:34:58 +0530

    Made homepage UI improvements #21 (#24)
    * Made homepage UI improvements #21
    
    * made the fix

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,23 +1,28 @@
 import React from "react";
 import HeroSvg from "../../assets/images/pockeSVG.svg";
-import { Box, Button, Grid, Typography } from "@mui/material";
+import { Box, Grid, Typography } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 
 function Home() {
   const navigate = useNavigate();
   return (
-    <main style={{ minHeight: "82vh", display: "flex" }}>
+    <main style={{ minHeight: "82vh", display: "flex", backgroundColor: '#D0BFFF' }}>
       <Grid
         container
-        spacing={2}
         justifyContent="center"
         alignItems="center"
         sx={{ margin: "1.5rem 0" }}
       >
         <Grid
           item
-          width={{ xs: 200, sm: 300, md: 400, lg: 600 }}
-          order={{ xs: 1, md: 2 }}
+          xs={12}
+          sm={12}
+          md={6}
+          lg={6}
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+          }}
         >
           <img style={{ maxWidth: "100%" }} src={HeroSvg} alt="HeroSvg" />
         </Grid>
@@ -27,81 +32,50 @@ function Home() {
           sm={12}
           md={6}
           lg={6}
-          order={{ xs: 2, md: 1 }}
-          alignSelf="center"
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
         >
-          <ul>
-            <Typography
-              variant="h2"
-              fontWeight={700}
-              // sx={{ fontSize: "2rem" }}
-              fontSize={{ xs: "2rem", md: "2.5", lg: "3rem" }}
-            >
-              Cutting-Edge
-            </Typography>
-            <Typography
-              variant="h2"
-              fontWeight={700}
-              // sx={{ fontSize: "2rem" }}
-              fontSize={{ xs: "2rem", md: "2.5", lg: "3rem" }}
-              gutterBottom
-            >
-              Pokémon Trading Card App
-            </Typography>
-
-            <li style={{ marginLeft: "2rem" }}>
-              {" "}
-              <Typography sx={{ fontSize: "1.25rem" }} variant="body2">
-                search with us
-              </Typography>
-            </li>
-            <li style={{ marginLeft: "2rem" }}>
-              <Typography sx={{ fontSize: "1.25rem" }} variant="body2">
-                find with us
-              </Typography>
-            </li>
-            <li style={{ marginLeft: "2rem" }}>
-              <Typography sx={{ fontSize: "1.25rem" }} variant="body2">
-                own with us
-              </Typography>
-            </li>
-          </ul>
+          <Typography
+            className="font"
+            variant="h2"
+            fontWeight={700}
+            fontFamily={'Poppins'}
+            fontSize={{ xs: "2rem", md: "2.5", lg: "3rem" }}
+            textAlign="center" // Center-align the text
+          >
+            Cutting-Edge <span><span className="my-special-element magic"><span className="magic-text">Pokémon</span></span></span> Trading Card App
+          </Typography>
+          {/* <div className="properties">
+            <div className="point">search with us</div>
+            <div className="point">find with us</div>
+            <div className="point">own with us</div>
+          </div> */}
           <Box
             sx={{
               display: "flex",
-              gap: "2rem",
+              flexDirection: "column", // Stack buttons vertically
+              alignItems: "center", // Center-align the buttons
               marginTop: "2rem",
-              // justifyContent: "center",
             }}
           >
-            <Button
-              onClick={() => navigate("/search")}
-              sx={{
-                color: "#fff",
-                background: "#000",
-                "&:hover": {
-                  bgcolor: "#000",
-                  opacity: 0.8,
-                },
-              }}
-              variant="contained"
-            >
-              Search Card
-            </Button>
-            <Button
-              onClick={() => navigate("/set-search")}
-              sx={{
-                color: "#000",
-                borderColor: "#000",
-                "&:hover": {
-                  borderColor: "#000",
-                  opacity: 0.5,
-                },
-              }}
-              variant="outlined"
-            >
-              Search Set
-            </Button>
+            <div className="btn-container">
+              <button
+                  className="btn"
+                  onClick={() => navigate("/search")}
+                  fontFamily={'Poppins'}
+                >
+                  Search Card
+                </button>
+              <button
+                onClick={() => navigate("/set-search")}
+                className="btn"
+              >
+                Search Set
+              </button>
+            </div>
           </Box>
         </Grid>
       </Grid>
