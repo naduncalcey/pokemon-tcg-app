5ccad7a4affa1ea52cac5a4f01cdb9a885f275d7
Shubh <shubhjhawar78@gmail.com>
2023-09-05 13:02:55 -0400

    made UI changes in homepage

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,19 +1,38 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import { HomeContainer } from "./Home.Styles";
 import { motion } from "framer-motion";
-import pokemon from "../../assets/images/charizard.png";
+import { pokemons } from "../../assets/images";
+import { Link } from "react-router-dom";
 
 function Home() {
   const textFadeInVariant = {
     hidden: { opacity: 0, x: -50 },
-    show: { opacity: 1, x: 0, transition: { duration: 1 } },
+    show: { opacity: 1, x: 0, transition: { duration: 1.25 } },
   };
 
   const imageFadeInVariant = {
     hidden: { opacity: 0, y: -50 }, // Initially hidden and moved up
-    show: { opacity: 1, y: 0, transition: { duration: 1 } }, // Fade in and move down to its original position
+    show: { opacity: 1, y: 0, transition: { duration: 1.25 } }, // Fade in and move down to its original position
   };
 
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      let randomIndex = currentImageIndex;
+      while(randomIndex === currentImageIndex)
+      {
+        randomIndex = Math.floor(Math.random() * pokemons.length);
+      }
+      setCurrentImageIndex(randomIndex);
+    }, 3000)
+    // will change every 3 seconds
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, [currentImageIndex])
+  
   return (
     <HomeContainer>
       <div className="box-container">
@@ -24,17 +43,21 @@ function Home() {
             variants={textFadeInVariant}
           >
             <h1 className="heading">
-              Pokemon TCG Search - Ultimate Trading Card Experience.
+              Pokemon TCG Search - Ultimate <p className="text-gradient">Trading Card</p> Experience.
             </h1>
             <p className="subtext">
               Built specifically to help collectors who are building Pokemon TCG
               master sets.
             </p>
             <div className="btn-layout">
-              <button className="btn-layout--primary">
-                Search Pokemon Cards
-              </button>
-              <button className="btn-layout--secondary">Set Search</button>
+              <Link to='/set-search'>
+                <button className="btn-layout--secondary">
+                  Search Pokemon Cards
+                </button>
+              </Link>
+              <Link to="/search">
+                <button className="btn-layout--secondary">Set Search</button>
+              </Link>
             </div>
           </motion.div>
         </div>
@@ -44,7 +67,7 @@ function Home() {
           variants={imageFadeInVariant}
           className="image"
         >
-          <img src={pokemon} alt="charizard" />
+          <img src={pokemons[currentImageIndex]} alt="pokemon" />
         </motion.div>
       </div>
     </HomeContainer>
